65b56ad06d276bc560daf6d231d57e657fcb1008
Amin NAIRI <nairi.amin@gmail.com>
2019-01-04 21:04:56 +0100

    :package: production build

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,12 @@ export interface ICommandParserOptions {
 }
 export declare class CommandParser {
     private options;
-    constructor();
+    private versionIdentifier;
+    private name;
+    constructor(name: string, versionIdentifier: string);
     option(name: string, description: string, noValueExpected?: boolean): this;
     parse(parameters?: string[]): ICommandParserOptions;
     help(): string;
+    version(): string;
 }
 export declare const NO_VALUE_EXPECTED: boolean;
